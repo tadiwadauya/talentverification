a102b4e2bba8dff721858d5ae0b8fc214a3b9e18
unknown <dauya1994@gmail.com>
2023-07-21 02:14:00 +0200

    import

--- talent/models.py
+++ talent/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+class Company(models.Model):
+    name = models.CharField(max_length=255)
+    date_of_registration = models.DateField()
+    registration_number = models.CharField(max_length=50)
+    address = models.CharField(max_length=255)
+    contact_person = models.CharField(max_length=255)
+    contact_phone = models.CharField(max_length=20)
+    email = models.EmailField()
+
+class Department(models.Model):
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+
+class Employee(models.Model):
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    employee_id = models.CharField(max_length=50, null=True, blank=True)
+    department = models.ForeignKey(Department, on_delete=models.CASCADE)
+    role = models.CharField(max_length=255)
+    date_started = models.DateField()
+    date_left = models.DateField(null=True, blank=True)
+    duties = models.TextField()
--- talent/views.py
+++ talent/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
+from .models import Company, Employee
+from .serializers import CompanySerializer, EmployeeSerializer
+
 
 # Create your views here.
+class CompanyAPIView(APIView):
+    def post(self, request):
+        serializer = CompanySerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class EmployeeAPIView(APIView):
+    def post(self, request):
+        serializer = EmployeeSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def put(self, request, employee_id):
+        employee = get_object_or_404(Employee, id=employee_id)
+        serializer = EmployeeSerializer(employee, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
